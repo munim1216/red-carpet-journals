36dcbc3d1855a6cd743d430cd5d0d2b440c5d429
RedCarpet <redcarpet@bths.edu>
2025-04-08 09:10:55 -0400

    Initial Commit

---
+++ RedCarpetJournals/RedCarpetJournalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RedCarpetJournalsApp.swift
+//  RedCarpetJournals
+//
+//  Created by Student on 4/8/25.
+//
+
+import SwiftUI
+
+@main
+struct RedCarpetJournalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
