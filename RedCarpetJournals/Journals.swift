4dbc97df5ee73d3a25a869a358875ebd39696de8
Jonathan <redcarpet@bths.edu>
2025-04-09 09:16:16 -0400

    Added navigation bar

---
+++ RedCarpetJournals/Journals.swift
@@ -0,0 +1,18 @@
+//
+//  Journals.swift
+//  RedCarpetJournals
+//
+//  Created by Student on 4/9/25.
+//
+
+import SwiftUI
+
+struct Journals: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Journals()
+}
---
+++ RedCarpetJournals/Settings.swift
@@ -0,0 +1,18 @@
+//
+//  Settings.swift
+//  RedCarpetJournals
+//
+//  Created by Student on 4/9/25.
+//
+
+import SwiftUI
+
+struct Settings: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Settings()
+}
