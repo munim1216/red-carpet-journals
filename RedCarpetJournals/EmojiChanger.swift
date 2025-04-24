45a04596850829738c4c248f4d00af0012d34101
RedCarpet <redcarpet@bths.edu>
2025-04-24 17:34:12 -0400

    final commit from me.

---
+++ RedCarpetJournals/EmojiChanger.swift
@@ -0,0 +1,18 @@
+//
+//  EmojiChanger.swift
+//  RedCarpetJournals
+//
+//  Created by Student on 4/24/25.
+//
+
+import SwiftUI
+
+struct EmojiChanger: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EmojiChanger()
+}
